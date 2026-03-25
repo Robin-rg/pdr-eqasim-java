536ab5cce2ff96ad2c1eefc50d948f81d4194f1f
Robin <148583910+Robin-rg@users.noreply.github.com>
2026-03-25 16:25:08 +0100

    Run : drs

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulation.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/RunSimulation.java
@@ -9,6 +9,15 @@ import org.matsim.core.config.ConfigUtils;
 import org.matsim.core.controler.Controler;
 import org.matsim.core.scenario.ScenarioUtils;
 
+/* == DRS == */
+import org.matsim.core.config.ConfigGroup;
+
+import at.ac.ait.matsim.drs.run.Drs;
+import at.ac.ait.matsim.drs.run.DrsConfigGroup;
+import at.ac.ait.matsim.drs.util.CarLinkAssigner;
+import at.ac.ait.matsim.drs.util.DrsUtil;
+/* ========= */
+
 public class RunSimulation {
 	static public void main(String[] args) throws ConfigurationException {
 		CommandLine cmd = new CommandLine.Builder(args) //
@@ -17,7 +26,7 @@ public class RunSimulation {
 				.build();
 
 		IDFConfigurator configurator = new IDFConfigurator(cmd);
-		Config config = ConfigUtils.loadConfig(cmd.getOptionStrict("config-path"));
+		Config config = ConfigUtils.loadConfig(cmd.getOptionStrict("config-path"), new ConfigGroup[]{new DrsConfigGroup()} );
 		configurator.updateConfig(config);
 
 		cmd.applyConfiguration(config);
@@ -28,8 +37,21 @@ public class RunSimulation {
 		ScenarioUtils.loadScenario(scenario);
 		configurator.adjustScenario(scenario);
 
+        /* == DRS == */
+        (new CarLinkAssigner(scenario.getNetwork())).run(scenario.getPopulation());
+        DrsUtil.addMissingCoordsToPlanElementsFromLinks(scenario.getPopulation(), scenario.getNetwork());
+        DrsUtil.addNewAllowedModeToCarLinks(scenario.getNetwork(), "drsDriver");
+
+        DrsUtil.addFakeGenericRouteToDrsDriverLegs(scenario.getPopulation());
+        /* ========= */
+
 		Controler controller = new Controler(scenario);
 		configurator.configureController(controller);
+
+		/* == DRS == */
+        Drs.prepareController(controller);
+        /* ========= */
+
 		controller.run();
 	}
 }
